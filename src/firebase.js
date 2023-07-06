350f18939bbec8063581cde4a5f76e657c1c227e
tykees <balogun.olu.sam@gmail.com>
2023-07-06 10:54:47 +0100

    env

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCkU-rF385gjq6EH9xD28olXEwBqCEH07I",
-//   authDomain: "decentrabuilds-e0101.firebaseapp.com",
-//   projectId: "decentrabuilds-e0101",
-//   storageBucket: "decentrabuilds-e0101.appspot.com",
-//   messagingSenderId: "272099182095",
-//   appId: "1:272099182095:web:1972596f66f56a651bc57a",
-//   measurementId: "G-KHPJYN7NRC"
-// };
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
